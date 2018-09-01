ba07d001e1a1bed2e34fe7ab5f52eaad76b558dc
Andi Muh. Rizqi <ikiganteng@gegsulbar.com>
2018-09-02 02:22:57 +0700

    Update story.php

--- story.php
+++ story.php
@@ -42,7 +42,7 @@ function story($cookie, $code){
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, 0);
 	if($data != null){
 	        curl_setopt($ch, CURLOPT_POST, 1);
         	curl_setopt($ch, CURLOPT_POSTFIELDS, "code=".$code."&cookie=".$cookie);
